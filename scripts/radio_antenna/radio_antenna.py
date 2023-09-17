652fa1f3af2fa14a51ea4448b53e984e6637ca8e
artemave <artemave@gmail.com>
2023-09-17 21:27:24 +0200

    added radio antenna

---
+++ scripts/radio_antenna/radio_antenna.py
@@ -0,0 +1,22 @@
+import math
+from mcpi import block
+import sys
+sys.path.insert(0, '')
+from utils.startPoint import * 
+sp = getStartPoint()
+
+mc.setBlocks(sp['x'], sp['y'], sp['z'], sp['x'], sp['y'] + 49, sp['z'], block.STONE.id)
+
+radius = 5 
+
+sp['y'] += 50
+
+for _ in range(5): 
+    for angle in range(0,360):
+        if angle <= 45 or (angle >= 135 and angle <= 225) or angle >= 315:
+            x = math.cos(math.radians(angle)) * radius + sp['x']
+            y = math.sin(math.radians(angle)) * radius + sp['y']
+
+            mc.setBlock(x,y,sp['z'],block.WOOL.id)
+
+    radius += 5
