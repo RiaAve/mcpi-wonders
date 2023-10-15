3f61c2b2341345193b4ababb6293db20d020f424
RiaAve <victoria.avetisyan42@gmail.com>
2023-10-15 17:45:42 +0200

    added sphere

--- scripts/radio_antenna/radio_antenna.py
+++ scripts/radio_antenna/radio_antenna.py
@@ -7,16 +7,19 @@ sp = getStartPoint()
 
 mc.setBlocks(sp['x'], sp['y'], sp['z'], sp['x'], sp['y'] + 49, sp['z'], block.STONE.id)
 
-radius = 5 
-
 sp['y'] += 50
 
-for _ in range(5): 
-    for angle in range(0,360):
-        if angle <= 45 or (angle >= 135 and angle <= 225) or angle >= 315:
-            x = math.cos(math.radians(angle)) * radius + sp['x']
-            y = math.sin(math.radians(angle)) * radius + sp['y']
+def drawDisk(hangle):
+    radius = 5 
 
-            mc.setBlock(x,y,sp['z'],block.WOOL.id)
+    for _ in range(5): 
+        for angle in range(0,360):
+            if angle <= 45 or (angle >= 135 and angle <= 225) or angle >= 315:
+                x = math.cos(math.radians(angle)) * radius + sp['x']
+                y = math.sin(math.radians(angle)) * radius + sp['y']
 
-    radius += 5
+                mc.setBlock(x,y,sp['z'],block.WOOL.id)
+
+        radius += 5
+
+drawDisk(45)
---
+++ scripts/sphere/sphere.py
@@ -0,0 +1,19 @@
+import math
+from mcpi import block
+import sys
+sys.path.insert(0, '')
+from utils.startPoint import * 
+sp = getStartPoint()
+
+def sphere(radius):
+
+    for hangle in range(360):
+        for vangle in range(360):
+            x = math.cos(math.radians(vangle)) 
+            y = math.sin(math.radians(vangle)) 
+            z = math.sin(math.radians(hangle)) 
+            nx = math.cos(math.radians(hangle)) * x
+            
+            mc.setBlock(nx * radius + sp['x'], y * radius + sp['y'] + 20, z * x * radius + sp['z'],block.GLOWSTONE_BLOCK.id)
+        
+sphere(25)
